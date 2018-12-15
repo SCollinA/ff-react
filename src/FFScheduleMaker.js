a0590a0e071b082ef2da0587b69d04261e36210f
Collin Argo <collinargo@gmail.com>
2018-12-14 21:51:32 -0500

    begin recursion level 1

--- src/FFScheduleMaker.js
+++ src/FFScheduleMaker.js
@@ -40,6 +40,13 @@ function makeTeams(numTeams) {
 
 // make random divisions
 function makeDivisions(numDivs, originalTeams) {
+    console.log('making divisions')
+    // make new divisions list
+    const divisions = makeDivisionsList(numDivs)
+    return assignTeams(divisions, originalTeams)
+}
+
+function makeDivisionsList(numDivs) {
     console.log('making divisions list')
     // make new divisions list
     let divisions = []
@@ -48,136 +55,89 @@ function makeDivisions(numDivs, originalTeams) {
         divisions.push([])
         numDivs--
     }
+    return divisions
+}
+
+function assignTeams(divisions, originalTeams) {
+    console.log('assigning teams')
+    const teamsPerDiv = originalTeams.length / divisions.length
     // make a copy of the teams array
     const teams = originalTeams.slice()
     // until all teams are added
     while (teams.length > 0) {
         // get a random team
         const randomTeamIndex = Math.floor(Math.random() * teams.length)
-        const randomTeam = teams.splice(randomTeamIndex)
+        const randomTeam = teams.splice(randomTeamIndex, 1)
+        console.log(randomTeam)
         // get a random division
-        const randomDivIndex = Math.floor(Math.random() * divisions.length)
-        // add team to division
-        divisions[randomDivIndex].push(randomTeam)
+        let randomDivIndex = Math.floor(Math.random() * divisions.length)
+        // check division fill level
+        while (divisions[randomDivIndex].length === teamsPerDiv) {
+            // if full pick a new index
+            randomDivIndex = Math.floor(Math.random() * divisions.length)
+        }
+        // if division is not full
+        if (divisions[randomDivIndex].length < teamsPerDiv) {
+            // add team to division
+            divisions[randomDivIndex].push(randomTeam)
+        } else {
+            
+        }
     }
     return divisions
-}
-
-function makeDivisionsList() {
-    
 }
 
 // this is the recursive function
 function makeSchedule(league) {
     recursionCounter = 0
     console.log(league)
-    const {schedule, teams, divisions, numWeeks} = league
-    console.log(`beginning recursion level ${++recursionCounter}, at ${league}`)
-    while (schedule.length < numWeeks || (schedule.length > 0 && schedule[schedule.length - 1].length < teams.length / 2)) {
-        // while it's empty, add random games
-        console.log(`whaaaa`)  
-        break
-    } 
+    let {schedule, teams, divisions, numWeeks} = league
+    console.log(`beginning recursion level ${++recursionCounter}`)
+    // if the schedule isn't full of weeks
+    if (schedule.length <= numWeeks) {
+        // if the schedule is empty
+        if (schedule.length === 0) {
+            // add a week
+            addWeek()
+            schedule = store.getState().schedule
+        } else {
+            // get the last week
+            const week = schedule[schedule.length - 1]
+            // if the week isn't full of games
+            if (week.length < teams.length / 2) {
+                // add a random game
+                addGame(randomGame(teams))
+                schedule = store.getState().schedule
+            } else {
+                // add another week
+                addWeek()
+                schedule = store.getState().schedule
+            }
+        }
+    }
+    // check schedule 
     // return schedule
+    return checkSchedule(league)
+}
 
+function randomGame(originalTeams) {
+    // make a copy of the teams list
+    const teams = originalTeams.slice()
+    // get one random team
+    let randomIndex = Math.floor(Math.random() * teams.length)
+    const randomTeam1 = teams.splice(randomIndex, 1)
+    randomIndex = Math.floor(Math.random() * teams.length)
+    const randomTeam2 = teams.splice(randomIndex, 1)
+    return [randomTeam1, randomTeam2]
+}
 
-
-    addWeek()
-    deleteWeek()
-    addGame()
-    deleteGame()
-    return [
-        [
-            ['Team1', 'Team2'],
-            ['Team3', 'Team4'],
-            ['Team5', 'Team6'],
-            ['Team7', 'Team8'],
-        ],
-        [
-            ['Team1', 'Team2'],
-            ['Team3', 'Team4'],
-            ['Team5', 'Team6'],
-            ['Team7', 'Team8'],
-        ],
-        [
-            ['Team1', 'Team2'],
-            ['Team3', 'Team4'],
-            ['Team5', 'Team6'],
-            ['Team7', 'Team8'],
-        ],
-        [
-            ['Team1', 'Team2'],
-            ['Team3', 'Team4'],
-            ['Team5', 'Team6'],
-            ['Team7', 'Team8'],
-        ],
-        [
-            ['Team1', 'Team2'],
-            ['Team3', 'Team4'],
-            ['Team5', 'Team6'],
-            ['Team7', 'Team8'],
-        ],
-        [
-            ['Team1', 'Team2'],
-            ['Team3', 'Team4'],
-            ['Team5', 'Team6'],
-            ['Team7', 'Team8'],
-        ],
-        [
-            ['Team1', 'Team2'],
-            ['Team3', 'Team4'],
-            ['Team5', 'Team6'],
-            ['Team7', 'Team8'],
-        ],
-        [
-            ['Team1', 'Team2'],
-            ['Team3', 'Team4'],
-            ['Team5', 'Team6'],
-            ['Team7', 'Team8'],
-        ],
-        [
-            ['Team1', 'Team2'],
-            ['Team3', 'Team4'],
-            ['Team5', 'Team6'],
-            ['Team7', 'Team8'],
-        ],
-        [
-            ['Team1', 'Team2'],
-            ['Team3', 'Team4'],
-            ['Team5', 'Team6'],
-            ['Team7', 'Team8'],
-        ],
-        [
-            ['Team1', 'Team2'],
-            ['Team3', 'Team4'],
-            ['Team5', 'Team6'],
-            ['Team7', 'Team8'],
-        ],
-        [
-            ['Team1', 'Team2'],
-            ['Team3', 'Team4'],
-            ['Team5', 'Team6'],
-            ['Team7', 'Team8'],
-        ],
-        [
-            ['Team1', 'Team2'],
-            ['Team3', 'Team4'],
-            ['Team5', 'Team6'],
-            ['Team7', 'Team8'],
-        ],
-        [
-            ['Team1', 'Team2'],
-            ['Team3', 'Team4'],
-            ['Team5', 'Team6'],
-            ['Team7', 'Team8'],
-        ],
-        [
-            ['Team1', 'Team2'],
-            ['Team3', 'Team4'],
-            ['Team5', 'Team6'],
-            ['Team7', 'Team8'],
-        ],
-    ]
+function checkSchedule(league) {
+    const {schedule, teams, divisions, numWeeks} = league
+    if (schedule.length === numWeeks && schedule[schedule.length - 1].length === teams.length / 2) {
+        return schedule
+    } else {
+        return makeSchedule(league)
+    }
 }
 
 function addWeek() {
