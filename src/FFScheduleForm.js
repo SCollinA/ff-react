4edd1893430d7c584f449693b09346be6eec1de3
Collin Argo <collinargo@gmail.com>
2018-12-17 09:33:40 -0500

    initial setup;

--- src/FFScheduleForm.js
+++ src/FFScheduleForm.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import addGames from './FFMaker'
 
 export default function FFScheduleForm(props) {
     return (
@@ -8,6 +9,7 @@ export default function FFScheduleForm(props) {
             console.log('in the ff schedule form')
             const { numPlayoffTeams, numDivs, numTeams } = event.target
             props.makeSchedule({numTeams: numTeams.value, numDivs: numDivs.value, numPlayoffTeams:  numPlayoffTeams.value})
+            props.addGames()
         }}
         >
             <label name="numTeams"># of teams</label>
--- src/store.js
+++ src/store.js
@@ -5,11 +5,35 @@ import { createStore } from 'redux';
 
 
 const defaultState = {
+    userSettings: {
+        numTeams: 8,
+        numDivs: 2,
+        numPlayoffTeams: 4
+    },
+    league: {
+        teams: [],
+        divisions: []
+    },
     schedule: []
 }
 
+
+
+
 const MAKE_SCHEDULE = {
     type: 'MAKE_SCHEDULE',
+}
+
+const ADD_TEAM = {
+    type: 'ADD_TEAM',
+}
+
+const ADD_DIVISION = {
+    type: 'ADD_DIVISION',
+}
+
+const ASSIGN_TEAM = {
+    type: 'ASSIGN_TEAM',
 }
 
 const ADD_WEEK = {
@@ -28,10 +52,54 @@ const DEL_GAME = {
     type: 'DEL_GAME',
 }
 
-export const makeSchedule = () => {
+export const makeSchedule = (userSettings) => {
     return {
         ...MAKE_SCHEDULE,
-        schedule: []
+        ...defaultState,
+        userSettings,
+    }
+}
+
+export const addTeam = (team) => {
+    return {
+        ...ADD_TEAM,
+        league: {
+            ...league,
+            teams: [
+                ...teams,
+                team
+            ]
+        }
+    }
+}
+
+export const addDivision = () => {
+    return {
+        ...ADD_DIVISION,
+        league: {
+            ...league,
+            divisions: [
+                ...divisions,
+                []
+            ]
+        }
+    }
+}
+
+export const assignTeam = ({team, divIndex}) => {
+    return {
+        ...ASSIGN_TEAM,
+        league: {
+            ...league,
+            divisions: [
+                ...divisions.slice(0, divIndex),
+                [
+                    ...divisions[divIndex],
+                    team
+                ],
+                ...divisions.slice(divIndex)
+            ]
+        }
     }
 }
 
@@ -75,7 +143,21 @@ const game = (state=defaultState, action) => {
     switch(action.type) {
         case MAKE_SCHEDULE.type:
             return {
-                schedule: action.schedule
+                league: action.league,
+                schedule: action.schedule,
+                userSettings: action.userSettings,
+            }
+        case ADD_TEAM.type:
+            return {
+                league: action.league
+            }
+        case ADD_DIVISION.type:
+            return {
+                league: action.league
+            }
+        case ASSIGN_TEAM.type:
+            return {
+                league: action.league
             }
         case ADD_WEEK.type:
             return {
